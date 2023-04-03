9409b162b7764f96a768b13eb0d6a42dfe030b86
hyonjnk@gmail.com <Pururin120>
2023-04-04 01:08:23 +0900

    レポジトリーを成果物用に変更

--- 구/README.md
+++
@@ -1,6 +0,0 @@
-# Web
-웹 개발관련 공부
-HTML
-CSS
-Javascript
-react
--- 구/Untitled-2.js
+++
@@ -1,5 +0,0 @@
-const mathScore = prompt("수학 몇점?");
-const engScore = prompt(`영어 몇점?`);
-const result = mathScore + engScore / 2;
-
-console.log(`평균` + result + `점 입니다.`);
